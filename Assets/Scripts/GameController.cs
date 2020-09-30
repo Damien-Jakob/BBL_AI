bd35e5ef0f7de54037bfba11ea8803f879f3a16d
Damien Jakob <damien.jakob@cpnv.ch>
2020-09-30 09:39:23 +0200

    Fix GameController Start

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,15 +7,16 @@ public class GameController : MonoBehaviour
 
     void Start()
     {
+        
         Coach coach = new HumanCoach();
         Team team = new Team(coach);
         Player player = new Player(team);
 
+        
         game = new Game();
         game.Team = team;
-        game.Pitch = GetComponent<PitchController>().Pitch;
+        game.Pitch = GameObject.FindGameObjectWithTag("Pitch").GetComponent<PitchController>().Pitch;
 
-        
         game.PutPlayer(player, 0, 3);
         game.PutBall(0, 1);
     }
