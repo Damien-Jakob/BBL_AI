b24005d44069cd09e697a3600c9bb28d7cffba11
Damien Jakob <damien.jakob@cpnv.ch>
2020-09-30 14:25:51 +0200

    Add second test player

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -13,16 +13,26 @@ public class GameController : MonoBehaviour
         // Temp data
         Coach coach = new HumanCoach();
         Team team = new Team(coach);
-        Player player = new Player(team);
+        Player testplayer1 = new Player(team);
+        Player testplayer2 = new Player(team);
 
         game = new Game();
         game.Team = team;
         game.Pitch = GameObject.FindGameObjectWithTag("Pitch").GetComponent<PitchController>().Pitch;
 
-        game.PutPlayer(player, 0, 3);
+        game.PutPlayer(testplayer1, 0, 3);
+        game.PutPlayer(testplayer2, 10, 10);
         game.PutBall(0, 1);
+
+        // ball test
+        // TODO shold be called by game.putball
         ballController.Put(0, 1);
         ballController.StartMovingTo(1, 2);
+
+        foreach(Player player in team.Players)
+        {
+            // TODO create player
+        }
     }
 
     // Update is called once per frame
