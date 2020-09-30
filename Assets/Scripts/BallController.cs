cccae1f618594a97f44d737c69842559c43b4b0d
Damien Jakob <damien.jakob@cpnv.ch>
2020-09-30 09:56:31 +0200

    Add ball controller

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BallController : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    Tilemap tilemap;
+
+    protected void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        tilemap = GameObject.FindGameObjectWithTag("GroundTilemap").GetComponent<Tilemap>();
+    }
+
+    public void Put(int positionX, int positionY)
+    {
+        Vector3Int position3D = new Vector3Int(positionX, positionY, 0);
+        Vector3 target = tilemap.CellToWorld(position3D);
+        transform.position = target;
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     Game game;
+    BallController ballController;
 
     void Start()
     {
-        
+        ballController = GameObject.FindGameObjectWithTag("Ball").GetComponent<BallController>();
+
+
         Coach coach = new HumanCoach();
         Team team = new Team(coach);
         Player player = new Player(team);
 
-        
         game = new Game();
         game.Team = team;
         game.Pitch = GameObject.FindGameObjectWithTag("Pitch").GetComponent<PitchController>().Pitch;
 
         game.PutPlayer(player, 0, 3);
         game.PutBall(0, 1);
+        ballController.Put(0, 1);
     }
 
     // Update is called once per frame
